36b2dd8a497adab44a84d55556472f0b4e3d09ed
TMD <kk412027247@gmail.com>
2018-11-19 14:33:32 +0800

    combine btc

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /** @format */
 
 import 'babel-polyfill'
-import {AppRegistry} from 'react-native';
+import './shim';
+import {AppRegistry, YellowBox} from 'react-native';
 // import App from './App';
-import App from './app2';
+import App from './app3';
 import {name as appName} from './app.json';
+YellowBox.ignoreWarnings = ([
+  'require.js:122 Require cycle',
+])
 
 AppRegistry.registerComponent(appName, () => App);
